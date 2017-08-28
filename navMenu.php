a7d9f32dcfb404040e10d50398ec34ea5032fad0
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-08-28 21:58:36 +0200

    skeleton for nav menu when you are logged in

--- navMenu.php
+++ navMenu.php
@@ -1,24 +1,19 @@
-
 <nav role="navigation" class="navbar navbar-default">
-    <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
         <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
-            <span class="sr-only">Toggle navigation</span>
+            <span class="sr-only">Task Planner</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a href="#" class="navbar-brand">Task Planner <i class="fa fa-clock-o"></i></a>
+        <a href="#" class="navbar-brand">Task Planner <i class="glyphicon glyphicon-edit"></i></a>
     </div>
-    <!-- Collection of nav links, forms, and other content for toggling -->
     <div id="navbarCollapse" class="collapse navbar-collapse navbar-right">
         <ul class="nav navbar-nav">
             <li <?php if ($page_name == "Home") echo 'class="active"'; ?> ><a href="index.php">Home</a></li>
-            <li <?php if ($page_name == "Activity") echo 'class="active"'; ?> ><a href="activity.php">Activity</a></li>
-            <li <?php if ($page_name == "Tracking") echo 'class="active"'; ?>><a href="tracking.php">Tracking</a></li>
-            <li <?php if ($page_name == "Report") echo 'class="active"'; ?>><a href="report.php">Share/Report</a></li>
+            <li <?php if ($page_name == "Activity") echo 'class="active"'; ?> ><a href="#">New Task</a></li>
+            <li <?php if ($page_name == "Tracking") echo 'class="active"'; ?>><a href="#">My tasks</a></li>
             <li><a href="logout.php">Log Out</a></li>
         </ul>
     </div>
 </nav>
-
